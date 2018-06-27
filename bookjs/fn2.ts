7c286d898d523ab1dbf0c793195eb3707e075c59
liuxiaoran <mk9540@163.com>
2018-06-27 17:40:58 +0800

    添加乘法的递归

--- bookjs/fn2.ts
+++ bookjs/fn2.ts
@@ -6,3 +6,15 @@ function fn(n){
 }
 
 console.log(fn(4));
+
+function fn2(n){
+  if(n===2){
+    return 4;
+  }else if(n===1){
+    return 2;
+  }else{
+    return fn2(n-1)*fn2(n-2);
+  }
+}
+
+console.log(fn2(4));
