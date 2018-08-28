d16c8db18e9cee16bd610b9d4b0ef5d33304c6e4
liu-xiaoran <mk9540@163.com>
2018-08-28 21:17:09 +0800

    定义箭头函数

---
+++ demoCode/jiantouFn.js
@@ -0,0 +1,12 @@
+(
+    () => {
+        console.log(123);
+    }
+)();
+
+(
+    function fn() {
+        console.log(123);
+    }
+)();
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Functions/Arrow_functions
