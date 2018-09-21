b1a5ca429fb3a6dd51fc141f221e970cac23db2e
liu-xiaoran <mk9540@163.com>
2018-09-21 18:00:04 +0800

    java开端

---
+++ java/hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    /* This is my first java program.
+    * This will print 'Hello World' as the output */
+    public static void main(String []args) { System.out.println("Hello World"); 
+    } 
+}
