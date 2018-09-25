1e3c6668c5e8f6a17ae0afd716cbca9a9c2d724e
liu-xiaoran <mk9540@163.com>
2018-09-25 15:08:45 +0800

    更新

---
+++ java/Job_Schedule_Test1.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+
+public class Job_Schedule_Test1 {
+    public static void main(String[] args) {
+        new JobThread().start();
+    }
+}
+
+class JobThread extends Thread {
+    public void run() {
+        while (true) {
+            System.out.println("Test: " + Calendar.getInstance().getTime());
+
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
