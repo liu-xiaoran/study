f6466e24a174edeeb100e186da062fb3b6422090
刘小然 <mk9540@.com>
2020-12-23 20:55:12 +0800

    修改dome

--- python/1.py
+++ python/1.py
@@ -1,3 +1,10 @@
-print(chr(97))
-print(chr(98))
-print(chr(99))
+# coding=utf8
+import time
+i=0
+lock = True
+while lock:
+    i+=1
+    time.sleep(0.2)
+    print("自动生成{}".format(i))
+    if i>66:
+        lock = False
